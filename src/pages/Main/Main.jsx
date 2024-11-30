af78517e460f3527f1580f1f3a7aee9824ebe4f8
Andrii Fedorov <sawwww7@gmail.com>
2024-11-30 16:16:49 +0200

    Hillel_react-projects

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,9 +1,33 @@
-import Form from "../../components/Form/Form";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
 
 const Main = () => {
+  const [yourFullName, setYourFullName] = useState("");
+
+  const navigate = useNavigate();
+  const handleFormVisible = () => {
+    navigate("/menu");
+  };
   return (
-    <div>
-      <Form />
+    <div className="main">
+      <h1>The best pizza.</h1>
+      <p className="subtitle">Straight out of the oven, straight to you.</p>
+      <p className="welcome">
+        👉 Welcome! Please start by telling us your name:
+      </p>
+      <Input
+        type={"text"}
+        className={"input"}
+        placeholder={"Your full name"}
+        aria_label={"Your full name"}
+        value={yourFullName}
+        setInputValue={setYourFullName}
+      />
+      <Button className={"btn"} onClick={handleFormVisible}>
+        Start Order
+      </Button>
     </div>
   );
 };
